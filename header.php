e7ced68fab172c8834a8d08081f2648a20ba83e9
YanSeg <yan.seguin@le-campus-numerique.fr>
2023-05-26 09:59:09 +0200

    ajout des fichier

---
+++ header.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="pragma" content="no-cache" />
+  <meta http-equiv="cache-control" content="max-age=604800" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="author" content="Bootstrap-ecommerce by Vosidiy" />
+
+  <title>BIG MECHA SHOP</title>
+
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+  <!-- jQuery -->
+  <script src="js/jquery-2.0.0.min.js" type="text/javascript"></script>
+
+  <!-- Bootstrap4 files-->
+  <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+  <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+
+  <!-- Font awesome 5 -->
+  <link href="fonts/fontawesome/css/fontawesome-all.min.css" type="text/css" rel="stylesheet" />
+
+  <!-- custom style -->
+  <link href="css/ui.css" rel="stylesheet" type="text/css" />
+  <link href="css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
+
+  <!-- custom javascript -->
+  <script src="js/script.js" type="text/javascript"></script>
+</head>
+
+<body>
+  <header class="section-header">
+    <nav class="navbar navbar-top navbar-expand-lg navbar-dark bg-secondary">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <!-- container //  -->
+    </nav>
+    <section class="header-main shadow">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-3 col-sm-4">
+            <div class="brand-wrap">
+              <img class="logo" src="images\logos\tetemecha.jpg" />
+              <h2 class="logo-text">BIG MECHA SHOP</h2>
+            </div>
+            <!-- brand-wrap.// -->
+          </div>
+          <div class="col-lg-6 col-sm-8">
+            <form action="#" class="search-wrap">
+              <div class="input-group w-100">
+                <input type="text" class="form-control" style="width:55%;" placeholder="Search" />
+                <select class="custom-select" name="category_name">
+                  <option value="">All type</option>
+                  <option value="codex">Special</option>
+                  <option value="comments">Only best</option>
+                  <option value="content">Latest</option>
+                </select>
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="submit">
+                    <i class="fa fa-search"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+            <!-- search-wrap .end// -->
+          </div>
+          <!-- col.// -->
+          <div class="col-lg-3 col-sm-12">
+            <a href="#" class="widget-header float-md-right">
+              <div class="icontext">
+                <div class="icon-wrap">
+                  <i class="flip-h fa-lg fa fa-phone"></i>
+                </div>
+                <div class="text-wrap">
+                  <small>Phone</small>
+                  <div>+97150 2813773</div>
+
+                </div>
+              </div>
+            </a>
+          </div>
+          <!-- col.// -->
+        </div>
+        <!-- row.// -->
+      </div>
+      <!-- container.// -->
+    </section>
+    <!-- header-main .// -->
+  </header>
+  <!-- section-header.// -->
---
+++ item.php
@@ -0,0 +1,12 @@
+<?php
+
+$Nom = "Imprimante3D";
+$Prix= 300;
+$URL = "https://pentaprint3d.fr/wp-content/uploads/2022/10/imprimante3d-zaxe-Z3.jpg" ;
+
+echo "$Nom";
+
+var_dump($Nom);
+?>
+
+
